8ffc0fc5b8239cf69e64b4d8e0af5a941bf6fce7
Paul <phunter510@gmail.com>
2018-12-24 21:53:39 -0600

    analysis addition

--- authorization_code/public/scripts/info.js
+++ authorization_code/public/scripts/info.js
@@ -1,6 +1,6 @@
 (function() {
 
-    /**
+    /*
      * Obtains parameters from the hash of the URL
      * @return Object
      */
@@ -11,18 +11,62 @@
 	while ( e = r.exec(q)) {
 	    hashParams[e[1]] = decodeURIComponent(e[2]);
 	}
-	console.log(hashParams); 
 	return hashParams;
     }
 
-    var userProfileSource = document.getElementById('user-profile-template').innerHTML,
+    // gets song features for top songs and
+    // songids is a comma separated string 
+    function getSongFeatures(songids, access_tok) {
+	$.ajax({
+	    url: 'https://api.spotify.com/v1/audio-features?ids=' + songids,
+	    headers: {
+		'Authorization': 'Bearer ' + access_tok
+	    },
+	    success: function(response) {
+		console.log(response); //temp
+
+		let features = response.audio_features; // Array of objects with feature data
+		let valenceAvg = 0,
+		    danceAvg = 0,
+		    energyAvg = 0;
+
+		let instrumentalCount = 0;
+
+		for (let i = 0; i < features.length; i++) {
+		    valenceAvg += features[i].valence;
+		    danceAvg += features[i].danceability;
+		    energyAvg += features[i].energy;
+		    if (features[i].instrumentalness > .7 ) {
+			instrumentalCount++;
+			console.log(i+1);
+		    }
+		}
+
+		valenceAvg /= features.length;
+		danceAvg /= features.length;
+		energyAvg /= features.length;
+
+		let featuresData = {valence: valenceAvg, danceability: danceAvg, energy: energyAvg,
+				    instrumentals: instrumentalCount};
+		console.log(featuresData);
+		featuresPlaceholder.innerHTML = featuresTemplate(featuresData);
+	    }
+	});
+    }
+    
+    let userProfileSource = document.getElementById('user-profile-template').innerHTML,
 	userProfileTemplate = Handlebars.compile(userProfileSource),
 	userProfilePlaceholder = document.getElementById('user-profile');
 
+    var featuresSource = document.getElementById('features-template').innerHTML,
+	featuresTemplate = Handlebars.compile(featuresSource),
+	featuresPlaceholder = document.getElementById('features');
+    
     var oauthSource = document.getElementById('oauth-template').innerHTML,
 	oauthTemplate = Handlebars.compile(oauthSource),
 	oauthPlaceholder = document.getElementById('oauth');
 
+    
     var params = getHashParams();
 
     var access_token = params.access_token,
@@ -39,8 +83,9 @@
 		refresh_token: refresh_token
 	    });
 
+	    // Request to spotify using access_token
 	    $.ajax({
-		url: 'https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50',  // Need to modify this
+		url: 'https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=50',  // Need to modify this
 		headers: {
 		    'Authorization': 'Bearer ' + access_token
 		},
@@ -49,6 +94,18 @@
 		    // with the data from the api call
 		    userProfilePlaceholder.innerHTML = userProfileTemplate(response);
 
+		    let topSongs = response.items;
+		    console.log(topSongs); // temporary
+		    let idList = '';
+		    
+		    for (let i = 0; i < topSongs.length; i++) {
+			idList += topSongs[i].id + ',';
+		    }
+
+		    console.log(idList) // temporary
+		    // request to get song information
+		    getSongFeatures(idList, access_token);
+		    		    		    
 		    $('#login').hide();
 		    $('#loggedin').show();
 		}
